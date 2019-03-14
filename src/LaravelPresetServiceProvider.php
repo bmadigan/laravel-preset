6b977f74c773af3111bf556eeb369771bc0a9a6a
Brad Madigan <bradmadigan@gmail.com>
2019-03-14 16:17:41 -0400

    WIP

--- src/LaravelPresetServiceProvider.php
+++ src/LaravelPresetServiceProvider.php
@@ -10,9 +10,7 @@ class LaravelPresetServiceProvider extends ServiceProvider
     public function boot()
     {
         PresetCommand::macro('bmadigan', function ($command) {
-            //Preset::install($command);
-            $command->info('its me');
-
+            Preset::install($command);
 //            $command->info('Preset scaffolding installed successfully.');
 //            $command->info('Please run "yarn && yarn run dev" to compile your fresh scaffolding.');
         });
